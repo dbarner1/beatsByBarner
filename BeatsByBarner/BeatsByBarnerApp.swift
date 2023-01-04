c08b07c83f5e0aafe52681589bae1b5dc22fce1d
Dave Barner <dbarner@gmail.com>
2023-01-04 11:44:38 -0500

    First commit

---
+++ BeatsByBarner/BeatsByBarnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatsByBarnerApp.swift
+//  BeatsByBarner
+//
+//  Created by Dave Barner on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BeatsByBarnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
